fcf34840e4c5084395ebb5aeb9d8b16078655467
Chang970605 <17721500831@163.com>
2021-12-01 21:10:18 +0800

    set model

--- 2dspp/main.py
+++ 2dspp/main.py
@@ -1,30 +1,54 @@
-import gurobipy as gb
+from gurobipy import *
 
 try:
-
+    
     # Create a new model
-    m = gb.Model("mip1")
+    m = Model("sspwp")
 
     # Create variables
-    x = m.addVar(vtype = gb.GRB.BINARY, name="x")
-    y = m.addVar(vtype = gb.GRB.BINARY, name="y")
-    z = m.addVar(vtype = gb.GRB.BINARY, name="z")
+    # 参数
+    N = 3     # 箱子数目
+    W = 10    # 容器宽
+    w = {}
+    h = {}
+    for i in range(1, N + 1):
+        w[i] = 2
+        h[i] = 4
+    
+
+    # 构建指标集
+    item_list = [i for i in range(1, N + 1)]
+    interactive_item_list = [(i, j) for i in range(1, N + 1) for j in range(1, N + 1) if i != j]
+    
+    # 决策变量
+    d = m.addVars(interactive_item_list, vtype= GRB.BINARY, name = "d")
+    l = m.addVars(interactive_item_list, vtype= GRB.BINARY, name = "l")
+    x = m.addVars(item_list, vtype= GRB.CONTINUOUS, name = "x")
+    y = m.addVars(item_list, vtype= GRB.CONTINUOUS, name = "y")
+    
+    # 参数
+    
+
+
 
     # Set objective
-    m.setObjective(x + y + 2 * z, gb.GRB.MINIMIZE)
+
 
     # Add constraint: x + 2 y + 3 z <= 4
-    m.addConstr(x + 2 * y + 3 * z <= 4, "c0")
-
+    
     # Add constraint: x + y >= 1
-    m.addConstr(x + y >= 1, "c1")
-
+    
+    
+    m.update()
+    print(w[1])
+    m.write('ssp.lp')
+    '''
     m.optimize()
 
     for v in m.getVars():
         print(v.varName, v.x)
 
     print('Obj:', m.objVal)
-
-except gb.GurobiError:
+    '''
+except GurobiError:
     print('Error reported')
---
+++ 2dspp/test.py
@@ -0,0 +1,6 @@
+w = {}
+h = {}
+for i in range(1, 3 + 1):
+    w[(i)] = 2
+    h[(i)] = 4
+print(h[1])
